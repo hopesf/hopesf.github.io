498524708a72fe3c04e4b3a66cb7133ddf894296
Hopesf <gecin18@gmail.com>
2022-06-26 18:56:03 +0300

    d

---
+++ _next/static/9jnDoPErAiAkrUELzbL9S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-72a7ee26acf944cb.js"],"/404":["static/chunks/pages/404-30fc4959ac113cda.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
